fdb15f753bd70b2e31ad1f58a40b668daf82aa07
Kenneth Oafallas <kenneth_oafallas@dlsu.edu.ph>
2025-07-05 21:40:14 +0800

    camera support

--- Main.cpp
+++ Main.cpp
@@ -11,6 +11,7 @@
 #include"VertexBuffer.h"
 #include"ElementArrayBuffer.h"
 #include"Texture.h"
+#include"Camera.h"
 
 int main()
 {
@@ -88,15 +89,12 @@ int main()
 	vertexBuffer.unbind();
 	elementArrayBuffer.unbind();
 
-	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
-
 	Texture texture("kotone256px.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	texture.texUnit(shaderProgram, "tex0", 0);
-	
-	float rotation = 0.0f;
-	double prevTime = glfwGetTime();
 
 	glEnable(GL_DEPTH_TEST);
+
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -105,31 +103,8 @@ int main()
 
 		shaderProgram.activate();
 
-		double currentTime = glfwGetTime();
-		if (currentTime - prevTime >= 1 / 60) {
-			rotation += 0.5f;
-			prevTime = currentTime;
-		}
-
-		glm::mat4 model = glm::mat4(1.0f);
-		glm::mat4 view = glm::mat4(1.0f);
-		glm::mat4 proj = glm::mat4(1.0f);
-
-		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.5f, 1.0f, 0.0f));
-		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
-		proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f);
-
-		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-
-		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
-
-
-		glUniform1f(uniID, 0.2f);
+		camera.inputs(window);
+		camera.matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
 
 		texture.bind();
 		vertexArray.bind();
